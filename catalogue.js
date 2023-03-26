0b4164e3bad6bf6e6e774004b55e6a2a9bf02153
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-03-26 01:26:50 +0100

    correction

--- catalogue.js
+++ catalogue.js
@@ -156,6 +156,7 @@ async function createCollection(page, name, browser, EANS, email){
     
 }
 
+
 process.on("message", async(message) => {
     let id = await run(message.EANS, message.name, message.body.email, message.headless);
     // send the results back to the parent process

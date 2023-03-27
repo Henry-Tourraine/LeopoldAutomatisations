4eb4b74920d092b25ac309cb3b801dbd5cb9e2b4
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-03-27 12:28:50 +0200

    correction

--- preview.js
+++ preview.js
@@ -5,7 +5,10 @@ async function preview(query){
     let browser = await chromium.launch({headless: true});
     let page = await browser.newPage();
     await page.goto("https://www.google.com/search?q="+query.split(" ").join("+"));
+    console.log("goto");
     let temp = await page.content();
+    console.log("page content");
+    browser.close();
     return temp;
   
   }
